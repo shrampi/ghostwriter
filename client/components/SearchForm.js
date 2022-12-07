45fb52a321cf001b11700cb3da326e543162d980
Carson Crow <carsoncrow@pm.me>
2022-12-07 15:22:14 -0600

    added catalog search functionality

---
+++ client/components/SearchForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchForm = ({onSubmit}) => {
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="search"
+          placeholder="Try title, author, subject, or gutenberg ID"
+          name="q"
+          spellCheck="true"
+        />
+        <button type='submit'>Search</button>
+      </form>
+        
+    </div>
+  )
+}
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func
+}
+
+export default SearchForm;
